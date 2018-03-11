4af4e203faf14aa71090e9f36d8f87df8e1fa665
rezaarrazi <rezaarrazi@gmail.com>
2018-03-11 23:36:20 +0700

    add source code

---
+++ KIJ_feistel/feistel/chipper/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace feistel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(255, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(158, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Plain text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Key word";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(86, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(158, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(255, 85);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Encode";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(340, 46);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 36);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Plain Text To Chiper";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Output";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(86, 54);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(158, 20);
+            this.textBox3.TabIndex = 14;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(255, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 133);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ KIJ_feistel/feistel/chipper/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace feistel
+{
+    public partial class Form1 : Form
+    {
+        int mode = 0;       //Variabel mode encode/decode
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Feistel
+
+        private static byte Encrypt(byte msg, Func<byte, byte, byte> FunctionF, byte[] keys)
+        {
+            byte step = msg;
+            for (int i = 0; i < keys.Length; i++)
+            {
+                step = FeistelStep(step, keys[i], FunctionF);
+            }
+
+            return step;
+        }
+
+        private static byte Decrypt(byte msg, Func<byte, byte, byte> FunctionF, byte[] keys)
+        {
+            byte step = msg;
+            step = InversionLR(step);
+            for (int i = keys.Length - 1; i >= 0; i--)
+            {
+                step = FeistelStep(step, keys[i], FunctionF);
+            }
+            step = InversionLR(step);
+
+            return step;
+        }
+
+        private static byte FunctionF(byte x, byte key)
+        {
+            return Xor(x, key);
+        }
+
+        private static byte FeistelStep(byte msg, byte key, Func<byte, byte, byte> FunctionF)
+        {
+            var R = GetR(msg);
+            var L = GetL(msg);
+
+            var funcResult = OperateR(R, key, FunctionF);
+
+            var xorResult = OperateL(L, funcResult, Xor);
+
+            var finalResult = InversionLR(xorResult, R);
+
+            return finalResult;
+        }
+
+        #endregion
+
+        #region Bit Manipulation
+
+        private static byte Xor(byte x, byte y)
+        {
+            return (byte)((int)x ^ (int)y);
+        }
+
+        private static byte GetR(byte x)
+        {
+            var temp = (byte)(((int)x) << 4);
+            return (byte)(((int)temp) >> 4);
+        }
+
+        private static byte GetL(byte x)
+        {
+            var temp = (byte)(((int)x) >> 4);
+            return (byte)(((int)temp) << 4);
+        }
+
+        private static byte InversionLR(byte l, byte r)
+        {
+            l = (byte)(((int)l) >> 4);
+            r = (byte)(((int)r) << 4);
+            return Xor(r, l);
+        }
+
+        private static byte InversionLR(byte msg)
+        {
+            var R = GetR(msg);
+            var L = GetL(msg);
+            return InversionLR(L, R);
+        }
+
+        private static byte OperateL(byte l, byte key, Func<byte, byte, byte> function)
+        {
+            key = (byte)(((int)key) << 4);
+            return function(l, key);
+        }
+
+        private static byte OperateR(byte r, byte key, Func<byte, byte, byte> function)
+        {
+            return function(r, key);
+        }
+
+        #endregion
+
+        #region Util
+
+        private static void PrintOutByte(string initialText, byte x)
+        {
+            var temp = (int)x;
+            int bits = 0;
+            int factor = 1;
+            for (int i = 7; i >= 0; i--)
+            {
+                bits += (temp % 2) * factor;
+                factor *= 10;
+                temp /= 2;
+            }
+            Console.WriteLine(initialText + "\t" + bits.ToString("00000000") + "\n");
+        }
+
+        private static byte EnsureKeyHas4Bits(byte key)
+        {
+            return (byte)((int)key % 16);
+        }
+
+        #endregion
+
+        #region Files
+
+        private static void EncryptFile(string fileInPath, string fileOutPath, Func<byte, byte, byte> FunctionF, byte[] keys)
+        {
+            byte[] file = File.ReadAllBytes(fileInPath);
+            byte[] encFile = new byte[file.Length];
+            for (int i = 0; i < file.Length; i++)
+            {
+                encFile[i] = Encrypt(file[i], FunctionF, keys);
+            }
+            string someString = Encoding.ASCII.GetString(encFile);
+            //Console.WriteLine("encrypt: " + someString);
+            File.WriteAllBytes(fileOutPath, encFile);
+        }
+
+        private static void DecryptFile(string fileInPath, string fileOutPath, Func<byte, byte, byte> FunctionF, byte[] keys)
+        {
+            byte[] file = File.ReadAllBytes(fileInPath);
+            byte[] decFile = new byte[file.Length];
+            for (int i = 0; i < file.Length; i++)
+            {
+                decFile[i] = Decrypt(file[i], FunctionF, keys);
+            }
+            File.WriteAllBytes(fileOutPath, decFile);
+        }
+
+        #endregion
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox2.Text))
+                MessageBox.Show("Fill the key!");
+            else if (string.IsNullOrWhiteSpace(textBox1.Text))
+                MessageBox.Show("No string to encode!");
+            else
+            {
+                string key = textBox2.Text;   //Variabel kata kunci
+                byte[] keys = Encoding.ASCII.GetBytes(key);
+
+                if (mode == 0)
+                {
+                    EncryptFile(textBox1.Text, textBox3.Text, FunctionF, keys);
+                }
+                else
+                {
+                    DecryptFile(textBox1.Text, textBox3.Text, FunctionF, keys);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (mode == 0)
+            {
+                mode = 1;
+                button4.Text = "Chiper To Plain Text";
+                label1.Text = "Chiper text";
+                //label3.Text = "Plain text";
+                button3.Text = "Decode";
+            }
+            else
+            {
+                mode = 0;
+                button4.Text = "Plain Text To Chiper";
+                label1.Text = "Plain text";
+                //label3.Text = "Chiper text";
+                button3.Text = "Encode";
+            }
+            //richTextBox1.Text = "";
+            //richTextBox2.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox3.Text = openFileDialog1.FileName;
+            }
+        }
+    }
+}
